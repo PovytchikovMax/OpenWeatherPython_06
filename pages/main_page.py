f2f64df9c760f3d99b9e5cb30f3db66ca223f72e
Alena Krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-08-06 08:21:05 +0300

    AT_003.10.20 Footer > Social media module > Icons-links visibility and сlickability > Verify image correctness in the Twitter brand-link (#1046)

--- pages/main_page.py
+++ pages/main_page.py
@@ -350,6 +350,11 @@ class MainPage(BasePage):
         image = self.find_element(self.locators.TWITTER_IMAGE)
         assert image.is_displayed(), "The image is not visible in the Twitter brand-link"
 
+    def check_image_is_correct_in_twitter_link(self):
+        image_src = self.find_element(self.locators.TWITTER_IMAGE).get_attribute("src")
+        assert image_src == FooterImageUrls.TWITTER_IMAGE_URL, \
+            "The image is not correct in the Twitter brand-link"
+
     def get_header_search_field_attribute(self, attribute):
         '''To retrieve the value of a specific attribute from Header Search field'''
         search_placeholder = self.driver.find_element(*self.locators.HEADER_SEARCH_FIELD)
