dfbe87c16078a519638bc7b42f77a6e79b151556
Alena Krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-08-10 22:03:37 +0300

    AT_003.10.24 Footer > Social media module > Icons-links visibility and сlickability > Verify image visibility in the Telergam brand-link (#1050)

--- tests/test_main_page_ow6.py
+++ tests/test_main_page_ow6.py
@@ -278,6 +278,10 @@ class TestMainPage:
         page = MainPage(driver)
         page.check_image_is_present_in_telegram_link()
 
+    def test_tc_003_10_24_verify_image_visibility_in_telegram_brand_link(self, driver, open_and_load_main_page):
+        page = MainPage(driver)
+        page.check_image_is_visible_in_telegram_link()
+
     def test_tc_003_12_01_check_historical_weather_data_link_functionality(self, driver, open_and_load_main_page):
         page = MainPage(driver)
         page.check_historical_weather_data_link_functionality()
