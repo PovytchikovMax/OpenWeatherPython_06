4a9912be8e9dd44d6113df1bc9d72b8cabbe2b15
Alena Krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-08-11 09:22:15 +0300

    AT_003.10.25 Footer > Social media module > Icons-links visibility and сlickability > Verify image correctness in the Telegram brand-link (#1051)

--- tests/test_main_page_ow6.py
+++ tests/test_main_page_ow6.py
@@ -282,6 +282,10 @@ class TestMainPage:
         page = MainPage(driver)
         page.check_image_is_visible_in_telegram_link()
 
+    def test_tc_003_10_25_verify_image_correctness_in_telegram_brand_link(self, driver, open_and_load_main_page):
+        page = MainPage(driver)
+        page.check_image_is_correct_in_telegram_link()
+
     def test_tc_003_12_01_check_historical_weather_data_link_functionality(self, driver, open_and_load_main_page):
         page = MainPage(driver)
         page.check_historical_weather_data_link_functionality()
