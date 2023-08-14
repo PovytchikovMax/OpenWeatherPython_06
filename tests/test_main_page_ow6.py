d8a977c83609afa944ad5cfad6d20136eeef0e33
Alena Krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-08-14 14:00:43 +0300

    AT_003.10.27 Footer > Social media module > Icons-links visibility and сlickability > Verify image visibility in the LinkedIn brand-link (#1053)

--- tests/test_main_page_ow6.py
+++ tests/test_main_page_ow6.py
@@ -290,6 +290,12 @@ class TestMainPage:
         page = MainPage(driver)
         page.check_image_is_present_in_linkedin_link()
 
+    def test_tc_003_10_27_verify_image_visibility_in_linkedin_brand_link(self, driver, open_and_load_main_page):
+        page = MainPage(driver)
+        linkedin_image = page.find_element(MainPageLocators.LINKEDIN_IMAGE)
+        page.go_to_element(linkedin_image)
+        page.check_element_image_is_visible(linkedin_image)
+
     def test_tc_003_12_01_check_historical_weather_data_link_functionality(self, driver, open_and_load_main_page):
         page = MainPage(driver)
         page.check_historical_weather_data_link_functionality()
