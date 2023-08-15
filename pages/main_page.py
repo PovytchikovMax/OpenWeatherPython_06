8ec084e8121be2ed446a4130a96ee2738bd73d68
Alena Krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-08-15 14:54:18 +0300

    AT_003.10.29 Footer > Social media module > Icons-links visibility and сlickability > Verify image presence in the Medium brand-link (#1055)

--- pages/main_page.py
+++ pages/main_page.py
@@ -380,10 +380,13 @@ class MainPage(BasePage):
         image = self.find_element(self.locators.LINKEDIN_IMAGE)
         assert image is not None, "The image is not present in the Linkedin brand-link"
 
+    def check_image_is_present_in_the_element(self, element):
+        assert element is not None, "The image is not present in the element"
+
     def check_element_image_is_visible(self, element):
         assert element.is_displayed(), "The image is not visible in the element"
 
-    def check_image_is_correct_in_linkedin_link(self, element, url):
+    def check_image_is_correct_in_the_link(self, element, url):
         image_src = element.get_attribute("src")
         assert image_src == url, "The image is not correct in the element"
 
