8c4e8cf605f95456b1a7e25b92594dbf4d0f4ced
Alena Krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-09-12 10:22:42 +0300

    AT_003.10.34 Footer > Social media module > Icons-links visibility and сlickability > Verify image correctness in the GitHub brand-link (#1076)

--- tests/test_main_page_ow6.py
+++ tests/test_main_page_ow6.py
@@ -355,6 +355,12 @@ class TestMainPage:
         page.go_to_element(github_image)
         page.check_element_image_is_visible(github_image)
 
+    def test_tc_003_10_34_verify_image_correctness_in_github_brand_link(self, driver, open_and_load_main_page):
+        page = MainPage(driver)
+        github_image = page.find_element(MainPageLocators.GITHUB_IMAGE)
+        github_image_url = FooterImageUrls.GITHUB_IMAGE_URL
+        page.check_image_is_correct_in_the_element(github_image, github_image_url)
+
     def test_tc_003_12_01_check_historical_weather_data_link_functionality(self, driver, open_and_load_main_page):
         page = MainPage(driver)
         page.check_historical_weather_data_link_functionality()
